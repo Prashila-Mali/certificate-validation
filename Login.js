889d7d363478b32c33da711817f674b20e9acc80
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2023-09-16 03:34:59 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,43 @@
+// Login.js
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement authentication logic here (e.g., call an authentication API).
+    // For simplicity, we'll just log the values.
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
